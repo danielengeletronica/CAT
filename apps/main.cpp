efea39b4526c49da3780bd0a9466cce3f7d95654
Daniel Lucas S. do Nascimento <60069848+danielengeletronica@users.noreply.github.com>
2020-06-16 10:02:25 -0300

    Update main.cpp

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,13 +1,6 @@
 #include "cat.h"
 #include <unistd.h>
 //g++ master.cpp -o master -lasiopal -lopendnp3 -lasiodnp3 -pthread -lopenpal
-#define PORT 20000 //porta para conectar o tcpserver e o master
-#define PORT1 20000 //porta para conectar o tcpClient com a outstation
-
-#define BUFFER_LENGTH 4096 //tamanho do string do payload
-
-/* Sockets buffers length */
-#define LEN 4096 //tamanho da string do payload
 
 /* endereços */
 #define SERVER_ADDR "127.0.0.1" //endereço da comunicaçao entre o tcpserver e o com master
